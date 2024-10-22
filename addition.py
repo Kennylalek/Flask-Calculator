b1b0d196b27eefbdf213cd7a35343ea78cffeb65
Meriemdemil <120749821+Meriemdemil@users.noreply.github.com>
2024-10-22 19:42:33 +0100

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+from flask import jsonify
 
-@app.route('/operation/add/<int:number1>/<int:number2>')
 def add(number1, number2):
     try:
         result = number1 + number2
